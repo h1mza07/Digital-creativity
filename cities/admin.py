8a22edfcd6b2b05558343faa7d74f1f498580ab1
h1mza07 <hamzakun37@gmail.com>
2026-01-13 04:01:55 +0100

    Hamza Layachi : Configuration Admin et interfaces

--- cities/admin.py
+++ cities/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import City
 
-# Register your models here.
+@admin.register(City)
+class CityAdmin(admin.ModelAdmin):
+    list_display = ('name', 'population', 'created_at')
+    search_fields = ('name', 'description')
+    list_filter = ('created_at',)
+    prepopulated_fields = {'slug': ('name',)}
+    readonly_fields = ('created_at', 'updated_at')
