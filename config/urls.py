4b5197a293de542d163dd44e4ba154e266e894ea
h1mza07 <hamzakun37@gmail.com>
2026-01-14 03:49:31 +0100

    Hamza Layachi: Validation des modèles et des relations entre les entités principales

--- config/urls.py
+++ config/urls.py
@@ -1,11 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
-<<<<<<< HEAD
 from django.conf import settings
 from django.conf.urls.static import static
 from django.conf.urls.i18n import i18n_patterns
 from django.views.generic import TemplateView
-=======
 from django.conf.urls import handler403, handler404
 from django.shortcuts import render
 
@@ -17,18 +15,20 @@ def error_404(request, exception):
 
 handler403 = error_403
 handler404 = error_404
->>>>>>> a99bebea1a853bc9ae4ade7d29dc1e1475c3dc78
 
 urlpatterns = [
     path('i18n/', include('django.conf.urls.i18n')),
     path('admin/', admin.site.urls),
-    path('users/', include('users.urls')),
 ]
 
 urlpatterns += i18n_patterns(
     path('', TemplateView.as_view(template_name='home.html'), name='home'),
+    
+    
     path('cities/', include('cities.urls')),
     path('worldcup/', include('worldcup2030.urls')),
+    path('users/', include('users.urls')),
+    
     prefix_default_language=False,
 )
 
