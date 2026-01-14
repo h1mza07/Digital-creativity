f87f71ecdd578da222deeda5f288c008b2e493d7
h1mza07 <hamzakun37@gmail.com>
2026-01-14 03:03:36 +0100

    Hamza Layachi: Ajouter Stadium à l'administration

--- worldcup2030/admin.py
+++ worldcup2030/admin.py
@@ -1,9 +1,16 @@
 from django.contrib import admin
-from .models import HostCity
+from .models import HostCity, Stadium  # ← AJOUTER Stadium
 
 @admin.register(HostCity)
 class HostCityAdmin(admin.ModelAdmin):
     list_display = ('city', 'is_host', 'stadium_capacity', 'matches_count')
     list_filter = ('is_host', 'created_at')
     search_fields = ('city__name', 'infrastructure_details')
+    readonly_fields = ('created_at', 'updated_at')
+
+@admin.register(Stadium)  # ← NOUVELLE CLASSE
+class StadiumAdmin(admin.ModelAdmin):
+    list_display = ('name', 'host_city', 'capacity', 'construction_year')
+    list_filter = ('construction_year', 'host_city')
+    search_fields = ('name', 'description', 'address')
     readonly_fields = ('created_at', 'updated_at')
