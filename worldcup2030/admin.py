c3f6186dda47b1cf8828fdea07a1d76786c9c29d
h1mza07 <hamzakun37@gmail.com>
2026-01-14 03:01:25 +0100

    Hamza Layachi : Configurer admin pour HostCity (villes hôtes)

--- worldcup2030/admin.py
+++ worldcup2030/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import HostCity
 
-# Register your models here.
+@admin.register(HostCity)
+class HostCityAdmin(admin.ModelAdmin):
+    list_display = ('city', 'is_host', 'stadium_capacity', 'matches_count')
+    list_filter = ('is_host', 'created_at')
+    search_fields = ('city__name', 'infrastructure_details')
+    readonly_fields = ('created_at', 'updated_at')
