cbaec62c28925d3bc0a81be8121f42779c68f133
h1mza07 <hamzakun37@gmail.com>
2026-01-14 02:14:09 +0100

    Hamza Layachi : Créer modèle City pour villes marocaines

--- cities/models.py
+++ cities/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+class City(models.Model):
+    name = models.CharField(_("Nom"), max_length=100)
+    slug = models.SlugField(_("Slug"), unique=True)
+    description = models.TextField(_("Description"))
+    image = models.ImageField(_("Image"), upload_to='cities/', null=True, blank=True)
+    latitude = models.FloatField(_("Latitude"))
+    longitude = models.FloatField(_("Longitude"))
+    population = models.IntegerField(_("Population"), null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    class Meta:
+        verbose_name = _("Ville")
+        verbose_name_plural = _("Villes")
+        ordering = ['name']
+    
+    def __str__(self):
+        return self.name
