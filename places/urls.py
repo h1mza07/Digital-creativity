5e96a8a6bf9be64c4c51674e715d098da7e404f4
Ibtissamainzura <ainzura.ibtissam@etu.uae.ac.ma>
2026-01-15 02:52:24 +0100

    création du fichier urls.py pour l´app places

---
+++ places/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from .views import PlaceListView, PlaceDetailView
+
+urlpatterns = [
+    path('', PlaceListView.as_view(), name='place_list'),
+    path('<int:pk>/', PlaceDetailView.as_view(), name='place_detail'),
+]
