f2e001fcd0e65db3abf13f331f19cbb380fd9255
EL Assioui Imane <elassiouimane08@gmail.com>
2026-01-15 13:38:10 +0100

    EL Assioui Imane : Ajout des vues hotel_list_by_city et hotel_detail dans hotels/views.py

--- hotels/views.py
+++ hotels/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Hotel
 
-# Create your views here.
+def hotel_list_by_city(request, city_id):
+    hotels = Hotel.objects.filter(city_id=city_id)
+    return render(request, 'hotels/hotel_list.html', {'hotels': hotels})
+
+def hotel_detail(request, hotel_id):
+    hotel = get_object_or_404(Hotel, id=hotel_id)
+    return render(request, 'hotels/hotel_detail.html', {'hotel': hotel})
