40c81e643110bc0834b8ea73d36cb3f9ced090aa
EL Assioui Imane <elassiouimane08@gmail.com>
2026-01-15 13:39:53 +0100

    EL Assioui Imane : Ajout des vues itinerary_list et itinerary_detail dans routes/views.py

--- routes/views.py
+++ routes/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Itinerary
 
-# Create your views here.
+def itinerary_list(request):
+    itineraries = Itinerary.objects.all()
+    return render(request, 'routes/itinerary_list.html', {'itineraries': itineraries})
+
+def itinerary_detail(request, itinerary_id):
+    itinerary = get_object_or_404(Itinerary, id=itinerary_id)
+    return render(request, 'routes/itinerary_detail.html', {'itinerary': itinerary})
