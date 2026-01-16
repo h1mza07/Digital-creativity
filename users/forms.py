94d82c841f96538e39454642100b89c5efe7cd46
aminachetti <amina.chetti@etu.uae.ac.ma>
2026-01-16 13:24:16 +0100

    Part 4.2: UserRegisterForm and ProfileForm

--- users/forms.py
+++ users/forms.py
@@ -1,14 +1,27 @@
 from django import forms
 from django.contrib.auth.models import User
-from django.contrib.auth.forms import UserCreationForm
 from .models import Profile
 
-class UserRegisterForm(UserCreationForm):
-    email = forms.EmailField()
+class UserRegisterForm(forms.ModelForm):
+    password1 = forms.CharField(label='Mot de passe', widget=forms.PasswordInput)
+    password2 = forms.CharField(label='Confirmer mot de passe', widget=forms.PasswordInput)
 
     class Meta:
         model = User
-        fields = ['username', 'email', 'password1', 'password2']
+        fields = ['username', 'email']
+
+    def clean(self):
+        cleaned_data = super().clean()
+        if cleaned_data.get("password1") != cleaned_data.get("password2"):
+            raise forms.ValidationError("Les mots de passe ne correspondent pas.")
+        return cleaned_data
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data["password1"])
+        if commit:
+            user.save()
+        return user
 
 
 class ProfileForm(forms.ModelForm):
