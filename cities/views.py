b2f3f9afc1be5f2356c4a7ce8464519a5180eeea
h1mza07 <hamzakun37@gmail.com>
2026-01-17 01:52:12 +0100

    Hamza Layachi : Modifier vue city_list pour utiliser la base de donnees

--- cities/views.py
+++ cities/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import City  
 
 def city_list(request):
-    return HttpResponse("<h1> Liste des villes marocaines</h1>")
+    cities = City.objects.all()  
+    return render(request, 'cities/city_list.html', {'cities': cities})  # REMPLACE CETTE LIGNE
 
 def city_detail(request, slug):
     return HttpResponse(f"<h1>Détails ville : {slug}</h1>")
