c6a3d6de49762cdd394b842fb66b6707f93b8f6a
h1mza07 <hamzakun37@gmail.com>
2026-01-17 04:46:02 +0100

    Hamza Layachi : Modifier vue city_detail pour afficher une ville specifique

--- cities/views.py
+++ cities/views.py
@@ -1,10 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
-from .models import City  
+from .models import City
+
+def home_page(request):
+    return render(request, 'home.html')
 
 def city_list(request):
-    cities = City.objects.all()  
-    return render(request, 'cities/city_list.html', {'cities': cities})  # REMPLACE CETTE LIGNE
+    cities = City.objects.all()
+    return render(request, 'cities/city_list.html', {'cities': cities})
 
 def city_detail(request, slug):
-    return HttpResponse(f"<h1>Détails ville : {slug}</h1>")
+    city = get_object_or_404(City, slug=slug)
+    return render(request, 'cities/city_detail.html', {'city': city})
