d779766a88911c55965274ac21805f345a47c468
El Assioui Imane <elassiouiimane08@gmail.com>
2026-01-19 13:40:57 +0100

    [Hotels] Recherche par mot-clé implémentée

--- hotels/views.py
+++ hotels/views.py
@@ -4,6 +4,8 @@ from .models import Hotel
 def hotel_list_by_city(request, city_id):
     hotels = Hotel.objects.filter(city_id=city_id)
     return render(request, 'hotels/hotel_list.html', {'hotels': hotels})
+if query:
+    hotels = hotels.filter(name__icontains=query)
 
 def hotel_detail(request, hotel_id):
     hotel = get_object_or_404(Hotel, id=hotel_id)
